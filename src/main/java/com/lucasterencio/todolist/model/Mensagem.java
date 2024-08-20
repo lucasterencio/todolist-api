63d3b5ae799e714de4cbc80727be61b117aed45f
Lucas Terêncio <lucas.tnsantos@upe.br>
2024-08-20 00:07:52 -0300

    Adicionando a entidade Task e criando a classe molde para tratar exceções

---
+++ src/main/java/com/lucasterencio/todolist/model/Mensagem.java
@@ -0,0 +1,16 @@
+package com.lucasterencio.todolist.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mensagem {
+    private String mensagem;
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
---
+++ src/main/java/com/lucasterencio/todolist/model/Task.java
@@ -0,0 +1,31 @@
+package com.lucasterencio.todolist.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255, updatable = true)
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
